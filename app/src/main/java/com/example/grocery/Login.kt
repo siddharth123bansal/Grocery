8caca6aa71c82399edc1d32ca44553c9d84347fd
Siddharth Bansal <chinubansal766@gmail.com>
2023-08-01 17:43:56 +0530

    Data added

---
+++ app/src/main/java/com/example/grocery/Login.kt
@@ -0,0 +1,34 @@
+package com.example.grocery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class Login : AppCompatActivity() {
+    lateinit var phonenum:EditText
+    lateinit var submit:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        phonenum=findViewById(R.id.editTextPhone)
+        submit=findViewById(R.id.button2)
+        submit.setOnClickListener {
+            if(phonenum.text.toString().isNotBlank() ||phonenum.text.toString().length==10 ){
+                login(phonenum.text.toString().trim())
+            }else{
+                Toast.makeText(this,"Enter a valid number",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun login(phonenum: String) {
+
+
+    }
+
+    fun showToast(message:String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/example/grocery/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.grocery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.shopNow).setOnClickListener {
+            startActivity(Intent(this,Login::class.java))
+        }
+    }
+}
